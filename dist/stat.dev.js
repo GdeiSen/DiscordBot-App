d9f1d02ba99dd43041bb8e1d1d625d0833e92bab
GdeiSen <75689548+GdeiSen@users.noreply.github.com>
2021-10-10 13:44:32 +0300

    Cleaning useless files

--- dist/stat.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var config = require("./config.json");
-
-module.exports = function (client) {
-  var guild = client.guilds.cache.get(config.guildid);
-  var people = guild.members.count;
-  console.log(people);
-};
--- stat.js
+++
@@ -1,6 +0,0 @@
-const config = require("./config.json");
-module.exports = function (client) {
-    const guild = client.guilds.cache.get(config.guildid);
-    let people = guild.members.count
-    console.log(people);
-}
