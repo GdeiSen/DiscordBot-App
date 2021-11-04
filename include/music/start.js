2757017c5d4524638b19e4e3de20ed7d605316e5
GdeiSen <gordeysenuta@gmail.com>
2021-11-04 22:25:21 +0300

    Global Music Update
    Improved some bugs and added the music-engine!

--- include/music/start.js
+++
@@ -1,7 +0,0 @@
-const { queueMaster } = require("./queueConstructor.js");
-const { songMaster } = require("./play.js");
-module.exports.run = async (client,message,args) => {
-    const serverQueueConstruct = new queueMaster(client,message);
-    serverQueueConstruct.createQueue();
-
-}
